8b93c1779f03ee8c206a5b3ebe904a3e6878b8be
Kaua-cel <kauaneres.dev@gmail.com>
2026-04-26 13:15:27 -0300

    feat: add config tailwind portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'brand-dark': '#05080a',
+        'brand-card': '#0b1218',
+        'brand-blue': '#1e40af',
+        'brand-accent': '#3b82f6',
+        'brand-muted': '#94a3b8',
+      }
+    }
+  }
+}
